d8b1d6fdfac3c37fa96cf3d3ea9629031beabdb2
Big Mac <randyhui123@gmail.com>
2025-11-27 16:10:17 -0800

    undo last commit

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Static files are served by Vercel from public/ folder
+// Serve static files (images, CSS, etc.)
+app.use(express.static(__dirname));
 
 // Explicit routes (optional since static middleware handles these)
 app.get('/', (req, res) => {
@@ -25,5 +26,6 @@ app.get('/header.html', (req, res) => {
 });
 
 
-// Export app for Vercel serverless deployment
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
